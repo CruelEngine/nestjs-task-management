6a6dec79ae587a13cdf7429b40be41a750c69ea7
CruelEngine <babhineethbhat@gmail.com>
2019-09-14 19:47:48 +0530

    task filtering added

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -3,6 +3,7 @@ import {Task, TaskStatus} from './task.model';
 import { } from 'uuid/v1';
 import uuid = require('uuid');
 import {CreateTaskDto} from './dto/create-task-dto';
+import {GetTasksFilterDto} from './dto/get-tasks-filter.dto';
 
 @Injectable()
 export class TasksService {
@@ -43,6 +44,23 @@ export class TasksService {
         const task = this.getTaskById(id);
         task.status = status;
         return task;
+    }
+
+
+    getTasksWithFilters(filterDto : GetTasksFilterDto) : Task[]{
+        const {status , search} = filterDto;
+
+        let tasks = this.getAllTasks();
+        if(status){
+            tasks = tasks.filter(task => task.status === status);
+        }
+        if(search){
+            tasks = tasks.filter(task =>
+                    task.title.includes(search) || task.description.includes(search)
+                );
+        }
+
+        return tasks;
     }
 
 
