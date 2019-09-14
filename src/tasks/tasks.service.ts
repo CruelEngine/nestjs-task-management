3b00310377ce07043e8ad6c15f64e28452f289e0
CruelEngine <babhineethbhat@gmail.com>
2019-09-14 17:03:38 +0530

    Task APIs and Service methods added

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,5 +1,8 @@
-import { Injectable } from '@nestjs/common';
-import {Task} from './task.model';
+import { Injectable, Get, Param } from '@nestjs/common';
+import {Task, TaskStatus} from './task.model';
+import { } from 'uuid/v1';
+import uuid = require('uuid');
+import {CreateTaskDto} from './dto/create-task-dto';
 
 @Injectable()
 export class TasksService {
@@ -7,6 +10,39 @@ export class TasksService {
 
     getAllTasks(): Task[] {
         return this.tasks;
+    }
+
+
+    createTask(createTaskDto : CreateTaskDto): Task{
+        const {title , description} = createTaskDto
+        const task : Task = {
+            id: uuid(),
+            title,
+            description,
+            status : TaskStatus.OPEN
+        }
+
+        this.tasks.push(task);
+        return task;
+    }
+
+
+
+    getTaskById( id : string):Task{
+        return this.tasks.find(task => task.id === id);
+    }
+
+
+    deleteTask(id : string) : void {
+       this.tasks =  this.tasks.filter(task => task.id !== id);
+        // return id;
+    }
+
+
+    updateTaskStatus(id : string , status : TaskStatus) : Task{
+        const task = this.getTaskById(id);
+        task.status = status;
+        return task;
     }
 
 
